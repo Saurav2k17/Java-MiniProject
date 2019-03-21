7e78c6af0504b9bcfaa00ccc3e721b7873cb0ebe
Saurav Kumar Sahu <sauravsahu@tamu.edu>
2019-03-21 00:11:53 -0500

    Constructed passenger class and created a template test class

---
+++ test/org/airline/reservations/PassengerTest.java
@@ -0,0 +1,34 @@
+package org.airline.reservations;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PassengerTest {
+
+	@Test
+	public void testPassenger() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPassengerString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
