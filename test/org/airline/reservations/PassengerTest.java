1e8010a07c568b91ec4be327c6b713241c26b6bf
Saurav Kumar Sahu <sauravsahu2009@gmail.com>
2019-03-23 18:15:10 -0500

    PassengerTest class fully implemented

--- test/org/airline/reservations/PassengerTest.java
+++ test/org/airline/reservations/PassengerTest.java
@@ -8,27 +8,28 @@ public class PassengerTest {
 
 	@Test
 	public void testPassenger() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testPassengerString() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetName() {
-		fail("Not yet implemented");
+		Passenger testPassenger1 = new Passenger();
+		String result = testPassenger1.getName();
+		assertEquals("Unknown name", result);
 	}
 
 	@Test
 	public void testSetName() {
-		fail("Not yet implemented");
+		Passenger testPassenger2 = new Passenger();
+		testPassenger2.setName("Mike Kelly");
+		assertEquals("Mike Kelly", testPassenger2.getName());
 	}
-
+	
+	@Test
+	public void testPassenger2() { //test second constructor
+		Passenger testPassenger3 = new Passenger("Mike Kelly");
+		assertEquals("Mike Kelly", testPassenger3.getName());
+	}
+	
 	@Test
 	public void testToString() {
-		fail("Not yet implemented");
+		Passenger testPassenger4 = new Passenger("Mike Kelly");
+		assertEquals("Passenger: Mike Kelly", testPassenger4.toString());
 	}
 
 }
